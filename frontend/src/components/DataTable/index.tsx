588099f0d45dbf2b6063dd7b58652fb0de44d5cd
Giovani Pacheco <gigio.sig@gmail.com>
2021-09-07 15:53:15 -0300

    Basic static components

---
+++ frontend/src/components/DataTable/index.tsx
@@ -0,0 +1,140 @@
+const DataTable = () => {
+  return (
+    <div className="table-responsive">
+      <table className="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>Data</th>
+            <th>Vendedor</th>
+            <th>Clientes visitados</th>
+            <th>Negócios fechados</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>23/06/2022</td>
+            <td>Jotape</td>
+            <td>23</td>
+            <td>31</td>
+            <td>32132.00</td>
+          </tr>
+          <tr>
+            <td>24/07/2022</td>
+            <td>Alek</td>
+            <td>32</td>
+            <td>13</td>
+            <td>3212.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>23/06/2022</td>
+            <td>Jotape</td>
+            <td>23</td>
+            <td>31</td>
+            <td>32132.00</td>
+          </tr>
+          <tr>
+            <td>24/07/2022</td>
+            <td>Alek</td>
+            <td>32</td>
+            <td>13</td>
+            <td>3212.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+          <tr>
+            <td>22/04/2021</td>
+            <td>Barry Allen</td>
+            <td>34</td>
+            <td>25</td>
+            <td>15017.00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DataTable;
